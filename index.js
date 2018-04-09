4115145287b43fed11541f3fdc333cebd48046e7
Li Ge <gelber.libi@gmail.com>
2018-04-09 06:29:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var recipes = {};
 Objects.assign({}, recipes);
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var obj = object.assign({}, { key: value });
+  Object.assign(object, { key: value });
+  var obj = 
   return obj;
 }
