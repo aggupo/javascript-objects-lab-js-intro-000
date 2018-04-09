2ded1f516361d66dced3950fe7938a1f2acd9b47
Li Ge <gelber.libi@gmail.com>
2018-04-09 06:32:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var obj = Object.assign(object, { key: value });
   return obj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  Object.assign(object, { key: value });
+  return object;
+}
